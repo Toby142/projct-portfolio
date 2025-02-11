b5b7e6e2076fdd9719e4657a449cb4eba4566ff0
Toby142 <tobiasvandeven142@gmail.com>
2025-02-11 22:35:50 +0100

    About me layout change and added age function

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -7,8 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AboutMeComponent {
 
+  age = this.getAge();
 
-
-
+  // fucntion that calculates my age from 14 februari 2005
+  getAge() {
+    const today = new Date();
+    const birthDate = new Date('2005-02-14');
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  }
+    
 
 }
